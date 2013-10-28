7db689c28a0e47bacc810ab96d1777d6dfb732d4
Pedro <peferre89@gmail.com>
2013-10-28 20:21:19 -0200

    alteracoes na classe maquina virtual, tirando o bloco de codigo do main

--- batalhaRobos/src/mv/MaquinaVirtual.java
+++ batalhaRobos/src/mv/MaquinaVirtual.java
@@ -1,30 +1,47 @@
 package mv;
 
-
 import java.util.Stack;
 import java.util.LinkedList;
 
 import mv.empilhaveis.*;
 import mv.instrucoes.*;
 
+
+
 public class MaquinaVirtual {
-    public static void main(String[] args) {
-	Stack<Empilhavel> pilhaDeDados = new Stack<Empilhavel>();
-	Memoria memoria = new Memoria();
-	Programas programas  = new Programas();
-	Instrucao codigo;
+
+	private Stack<Empilhavel> pilhaDeDados;
+	private Memoria memoria;
+	private Programas programas;
+	private Instrucao codigo;
 	LinkedList<Programa> listaDeProgramas;
 	
-	programas.setListaDeProgramasPadroes();
-	listaDeProgramas = programas.getListaDeProgramas();
-	for (Programa prg : listaDeProgramas) {
-	    codigo = prg.getInstrucao();
-	    while (!(codigo instanceof END)) {
-		codigo.executar(pilhaDeDados, memoria, prg);
-		codigo = prg.getInstrucao();
-	    }
+	void passo(){
+		
 	}
+	
+	void rodaMaquina() {
+		pilhaDeDados = new Stack<Empilhavel>();
+	    memoria = new Memoria();
+		programas = new Programas();
+		
 
-    }
+		programas.setListaDeProgramasPadroes();
+		listaDeProgramas = programas.getListaDeProgramas();
+		
+		for (Programa prg : listaDeProgramas) {
+			codigo = prg.getInstrucao();
+			while (!(codigo instanceof END)) {
+				codigo.executar(pilhaDeDados, memoria, prg);
+				codigo = prg.getInstrucao();
+			}
+		}
+
+	}
+	
+	public static void main(String[] args) {
+			MaquinaVirtual m = new MaquinaVirtual();
+			m.rodaMaquina();
+	}
 
 }
