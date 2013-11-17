689bfa7aad1c2a460bc5d8bd9f469bd5f2c94e6f
Pedro <peferre89@gmail.com>
2013-11-17 02:31:01 -0200

    adicionei imagens para base, como se fosse pontuaçoes

--- fase3/batalhaRobos-recolha/src/mundo/elementos/Base.java
+++ fase3/batalhaRobos-recolha/src/mundo/elementos/Base.java
@@ -7,16 +7,28 @@ import java.awt.Graphics;
 public class Base implements Posicionavel {
 	int equipe;
 	int numCristais;
-	private Sprite sprite;
+	private Sprite[] sprite;
 
 	public Base(int equipe){
 		this.equipe = equipe;
-		this.sprite = graficos.SpriteStore.get().getSprite("graficos/base.png");
+		this.sprite = new Sprite[8];
+		this.sprite[0] = graficos.SpriteStore.get().getSprite("graficos/base.png");
+		this.sprite[1] = graficos.SpriteStore.get().getSprite("graficos/base1.png");
+		this.sprite[2] = graficos.SpriteStore.get().getSprite("graficos/base2.png");
+		this.sprite[3] = graficos.SpriteStore.get().getSprite("graficos/base3.png");
+		this.sprite[4] = graficos.SpriteStore.get().getSprite("graficos/base4.png");
+		this.sprite[5] = graficos.SpriteStore.get().getSprite("graficos/base5.png");
+		this.sprite[6] = graficos.SpriteStore.get().getSprite("graficos/base6.png");
+		this.sprite[7] = graficos.SpriteStore.get().getSprite("graficos/base7.png");
 		}
 	
 	public void draw(Graphics g, int x, int y) {
-		sprite.draw(g, x-25, y-25);
-		
+		if(numCristais < 8){
+			sprite[numCristais].draw(g, x-25, y-25);
+		}
+		else
+			sprite[7].draw(g, x-25, y-25);
+			
 	}
 
 	public void addCristal(){
